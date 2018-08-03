9304c7873caf3209a4e492c21ead6c4ce9828b88
Geoff Cureton <geoff.cureton@ssec.wisc.edu>
2018-08-03 06:29:29 +0000

    Updated hirs_ctp_daily cluster submission script.

--- submit_hirs_ctp_daily.py
+++ submit_hirs_ctp_daily.py
@@ -20,7 +20,7 @@ from timeutil import TimeInterval, datetime, timedelta
 
 import flo.sw.hirs_ctp_orbital as hirs_ctp_orbital
 import flo.sw.hirs_ctp_daily as hirs_ctp_daily
-from flo.sw.hirs.utils import setup_logging
+from flo.sw.hirs2nc.utils import setup_logging
 
 # every module should have a LOG object
 LOG = logging.getLogger(__name__)
@@ -28,18 +28,41 @@ LOG = logging.getLogger(__name__)
 setup_logging(2)
 
 # General information
-hirs_version = 'v20151014'
-collo_version = 'v20151014'
-csrb_version = 'v20150915'
-ctp_version = 'v20150915'
+hirs2nc_delivery_id = '20180410-1'
+hirs_avhrr_delivery_id = '20180505-1'
+hirs_csrb_daily_delivery_id  = '20180714-1'
+hirs_csrb_monthly_delivery_id  = '20180516-1'
+hirs_ctp_orbital_delivery_id  = '20180730-1'
+hirs_ctp_daily_delivery_id  = '20180802-1'
 wedge = timedelta(seconds=1.)
 day = timedelta(days=1.)
+hour = timedelta(hours=1.)
 
 # Satellite specific information
+
+#satellite = 'noaa-07'
+#granule = datetime(2015, 4, 17, 0, 20)
+#intervals = [TimeInterval(granule, granule + wedge - wedge)]
+# NSS.GHRR.NC.D81236.S0013.E0207.B0086970.WI.level2.hdf --> NSS.GHRR.NC.D85032.S2221.E0009.B1863132.GC.level2.hdf
+# datetime(1981, 8, 24, 0, 13) --> datetime(1985, 2, 1, 0, 9)
+#intervals = []
+#for years in range(1981, 1986):
+    #intervals += [TimeInterval(datetime(years,month,1), datetime(years,month,calendar.monthrange(years,month)[1])+day-wedge) for month in range(1,13) ]
+
+#satellite = 'noaa-09'
+#granule = datetime(2015, 4, 17, 0, 20)
+#intervals = [TimeInterval(granule, granule + wedge - wedge)]
+# NSS.GHRR.NF.D85056.S0013.E0154.B0105253.GC.level2.hdf --> NSS.GHRR.NF.D88311.S2319.E0107.B2011011.GC.level2.hdf
+# datetime(1985, 2, 25, 0, 13) --> datetime(1988, 11, 6, 1, 7)
+#intervals = []
+#for years in range(1985, 1989):
+    #intervals += [TimeInterval(datetime(years,month,1), datetime(years,month,calendar.monthrange(years,month)[1])+day-wedge) for month in range(1,13) ]
 
 #satellite = 'noaa-19'
 #granule = datetime(2015, 4, 17, 0, 20)
 #intervals = [TimeInterval(granule, granule + wedge - wedge)]
+# NSS.GHRR.NP.D09108.S2301.E0050.B0100809.GC.level2.hdf --> NSS.GHRR.NP.D17365.S2238.E2359.B4585757.GC.level2.hdf
+# datetime(2009, 4, 18, 0, 0) --> datetime(2017, 12, 31, 0, 0)
 #intervals = []
 #years = 2009
 #intervals += [TimeInterval(datetime(years,month,1), datetime(years,month,calendar.monthrange(years,month)[1])+day-wedge) for month in range(4,13) ]
@@ -49,43 +72,40 @@ day = timedelta(days=1.)
 #intervals += [TimeInterval(datetime(years,month,1), datetime(years,month,calendar.monthrange(years,month)[1])+day-wedge) for month in range(1,2) ]
 
 satellite = 'metop-b'
-#granule = datetime(2017, 1, 1)
-#intervals = [TimeInterval(granule, granule + day - wedge)]
+#granule = datetime(2015,5,14, 9, 23)
+#intervals = [TimeInterval(granule, granule + hour - wedge)]
+# NSS.GHRR.M1.D13140.S0029.E0127.B0347172.SV.level2.hdf --> NSS.GHRR.M1.D15365.S2307.E0004.B1705253.SV.level2.hdf
+# datetime(2013, 5, 20, 0, 0) --> datetime(2017, 12, 31, 0, 0)
 intervals = []
-#intervals += [TimeInterval(datetime(years,month,1), datetime(years,month,calendar.monthrange(years,month)[1])+day-wedge) for month in range(10,13) ]
-#for years in range(2013,2018):
-    #intervals += [TimeInterval(datetime(years,month,1), datetime(years,month,calendar.monthrange(years,month)[1])+day-wedge) for month in range(1,13) ]
-years = 2017
-intervals += [TimeInterval(datetime(years,month,1), datetime(years,month,calendar.monthrange(years,month)[1])+day-wedge) for month in range(8,12) ]
+years = 2013
+intervals += [TimeInterval(datetime(years,month,1), datetime(years,month,calendar.monthrange(years,month)[1])+day-wedge) for month in range(5,13) ]
+for years in range(2014,2018):
+    intervals += [TimeInterval(datetime(years,month,1), datetime(years,month,calendar.monthrange(years,month)[1])+day-wedge) for month in range(1,13) ]
 
-# Data locations for each file type
-collection = {'HIR1B': 'ILIAD',
-              'CFSR': 'DELTA', # obsolete
-              'PTMSX': 'ILIAD'}
-
-# NOAA-19
-#input_data = {'HIR1B': '/mnt/sdata/geoffc/HIRS_processing/data_lists/NOAA-19/HIR1B_noaa-19_latest',
-              #'CFSR':  '/mnt/sdata/geoffc/HIRS_processing/data_lists/CFSR.out',
-              #'PTMSX': '/mnt/sdata/geoffc/HIRS_processing/data_lists/NOAA-19/PTMSX_noaa-19_latest'}
-
-# Metop-B
-input_data = {'HIR1B': '/mnt/cephfs_data/geoffc/hirs_data_lists/Metop-B/HIR1B_metop-b_latest',
-              'CFSR':  '/mnt/cephfs_data/geoffc/hirs_data_lists/CFSR.out',
-              'PTMSX': '/mnt/cephfs_data/geoffc/hirs_data_lists/Metop-B/PTMSX_metop-b_latest'}
-
-input_sources = {'collection':collection, 'input_data':input_data}
-
-# Initialize the hirs_ctp_daily module with the data locations
-hirs_ctp_daily.set_input_sources(input_sources)
-
-# Instantiate the computation
-# General information
-hirs_ctp_orbital_comp = hirs_ctp_orbital.HIRS_CTP_ORBITAL()
-comp = hirs_ctp_daily.HIRS_CTP_DAILY()
-
-platform_choices = ['noaa-06', 'noaa-07', 'noaa-08', 'noaa-09', 'noaa-10', 'noaa-11',
+satellite_choices = ['noaa-06', 'noaa-07', 'noaa-08', 'noaa-09', 'noaa-10', 'noaa-11',
                     'noaa-12', 'noaa-14', 'noaa-15', 'noaa-16', 'noaa-17', 'noaa-18',
                     'noaa-19', 'metop-a', 'metop-b']
+
+def setup_computation(satellite):
+
+    input_data = {'HIR1B': '/mnt/software/flo/hirs_l1b_datalists/{0:}/HIR1B_{0:}_latest'.format(satellite),
+                  'CFSR':  '/mnt/cephfs_data/geoffc/hirs_data_lists/CFSR.out',
+                  'PTMSX': '/mnt/software/flo/hirs_l1b_datalists/{0:}/PTMSX_{0:}_latest'.format(satellite)}
+
+    # Data locations
+    collection = {'HIR1B': 'ILIAD',
+                  'CFSR': 'DELTA',
+                  'PTMSX': 'FJORD'}
+
+    input_sources = {'collection':collection, 'input_data':input_data}
+
+    # Initialize the hirs_csrb_daily module with the data locations
+    hirs_ctp_daily.set_input_sources(input_sources)
+
+    # Instantiate the computation
+    comp = hirs_ctp_daily.HIRS_CTP_DAILY()
+
+    return comp
 
 LOG.info("Submitting intervals...")
 
@@ -101,7 +121,12 @@ try:
     for interval in intervals:
         LOG.info("Submitting interval {} -> {}".format(interval.left, interval.right))
 
-        contexts = comp.find_contexts(interval, satellite, hirs_version, collo_version, csrb_version, ctp_version)
+        comp = setup_computation(satellite)
+        hirs_ctp_orbital_comp = hirs_ctp_orbital.HIRS_CTP_ORBITAL()
+
+        contexts = comp.find_contexts(interval, satellite, hirs2nc_delivery_id, hirs_avhrr_delivery_id,
+                                      hirs_csrb_daily_delivery_id, hirs_csrb_monthly_delivery_id,
+                                      hirs_ctp_orbital_delivery_id, hirs_ctp_daily_delivery_id)
 
         LOG.info("Opening log file {}".format(log_name))
         file_obj = open(log_name,'a')
@@ -133,7 +158,7 @@ try:
             except Exception:
                 LOG.warning(traceback.format_exc())
 
-            #sleep(30.)
+            #sleep(10.)
 
         LOG.info("Closing log file {}".format(log_name))
         file_obj.close()
